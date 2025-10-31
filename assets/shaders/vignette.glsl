569e7c1fb07c1560b63c07b1ffc8cb3098e56067
Charlie <charlieraewebster@gmail.com>
2025-10-31 23:02:00 +0000

    Fixed torch shape - Charlie

--- assets/shaders/vignette.glsl
+++ assets/shaders/vignette.glsl
@@ -4,30 +4,40 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 uniform vec2 u_lightPos;
+uniform vec2 u_borderSize;
 uniform sampler2D u_texture;
 
 varying vec4 v_color;
 varying vec2 v_texCoords;
 
-const float outerRadius = 0.3;
+const float outerRadius = 0.2;
 const float innerRadius = 0.01;
 const float intensity = 1.0;
 
+
 void main() {
+    float aspectRatio = u_resolution.x / u_resolution.y;
+
     vec4 color = texture2D(u_texture, v_texCoords) * v_color;
 
-    // Shader needs 0-1 coords
-    vec2 uv = gl_FragCoord.xy / u_resolution;
+    // Correct light position for aspect ratio
+    vec2 correctedLightPos = vec2(u_lightPos.x * aspectRatio, u_lightPos.y);
+
+    // Change to 0-1 coords for fragment position
+    vec2 decimalCoords = gl_FragCoord.xy / u_resolution;
+
+    // Correct for resolution
+    vec2 correctedCoords = vec2(decimalCoords.x * (aspectRatio), decimalCoords.y);
 
     // Calculate distance to centre
-    float len = length(uv - u_lightPos);
+    float len = length(correctedCoords - correctedLightPos);
 
-    // Set vignette smoothness
+    // Set smoothness
     float vignette = smoothstep(outerRadius, innerRadius, len);
 
-    // Darken the image according to the sahder
+    // Apply the vignette
     color.rgb = mix(color.rgb, color.rgb * vignette, intensity);
 
-
     gl_FragColor = color;
+
 }
